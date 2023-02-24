450b70427c2c0ba4bcd221d87e2d597f7f0aead5
Eva Göttert <eva.goettert@idealo.de>
2023-02-24 15:13:02 +0100

    refactor Ohce class

--- src/main/kotlin/com/kata/Ohce.kt
+++ src/main/kotlin/com/kata/Ohce.kt
@@ -1,23 +1,17 @@
 package com.kata
 
 class Ohce {
-    private fun reverseWord(word: String): String {
-        return word.reversed()
-    }
-
-    fun isPalindrome(word: String): Boolean{
-        return word == reverseWord(word)
-    }
-
-    private fun greeting(name: String): String {
-        return "¡Buenos días $name!"
-    }
-
     fun generateOutput(input: String): String {
-
         return if (input.startsWith("ohce "))
             greeting(input.split(" ")[1])
         else if(isPalindrome(input)) "${input}\n¡Bonita palabra!"
-            else reverseWord(input)
+        else reverseWord(input)
     }
+    private fun reverseWord(word: String) = word.reversed()
+
+    private fun isPalindrome(word: String) = word == reverseWord(word)
+
+    private fun greeting(name: String) = "¡Buenos días $name!"
+
+
 }
