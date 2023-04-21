c97beb4987a3a790792cd9537324539083cab65f
Eva Göttert <eva.goettert@idealo.de>
2023-04-21 15:07:07 +0200

    Reformat code and add TODOs for next time

--- src/main/kotlin/com/kata/Wardrobe.kt
+++ src/main/kotlin/com/kata/Wardrobe.kt
@@ -3,19 +3,19 @@ package com.kata
 import com.kata.Size.TINY
 
 class Wardrobe {
-    fun getAll250CmCombinations() : List<List<Size>> {
-        val resultList =  mutableListOf<List<Size>>()
+    fun getAll250CmCombinations(): List<List<Size>> {
+        val resultList = mutableListOf<List<Size>>()
         val candidate = mutableListOf<Size>()
 
         candidate.add(TINY)
         var totalSize = candidate.sumBy { it.size }
 
-        while (totalSize<250){
+        while (totalSize < 250) {
             candidate.add(TINY)
             totalSize = candidate.sumBy { it.size }
         }
 
-        if (totalSize == 250){
+        if (totalSize == 250) {
             resultList.add(candidate)
         }
 
@@ -23,7 +23,7 @@ class Wardrobe {
     }
 }
 
-enum class Size (val size: Int, val price: Int){
+enum class Size(val size: Int, val price: Int) {
     TINY(50, 59),
     SMALL(75, 62),
     MEDIUM(100, 90),
--- src/test/kotlin/com/kata/WardrobeTest.kt
+++ src/test/kotlin/com/kata/WardrobeTest.kt
@@ -2,12 +2,19 @@ package com.kata
 
 import com.kata.Size.TINY
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Test
 
 internal class WardrobeTest {
 
-    @Test fun `find 250 sized combinations`() {
+    @Test
+    fun `find 250 sized combinations`() {
         val greeting = Wardrobe().getAll250CmCombinations()
-        assertThat(greeting).contains(listOf(TINY,TINY,TINY,TINY,TINY))
+        assertThat(greeting).contains(listOf(TINY, TINY, TINY, TINY, TINY))
     }
+
+    // TODO find all candidates
+
+    // TODO refactor to usage of recursion
+
+    //TODO prices
 }
