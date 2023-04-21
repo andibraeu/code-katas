9f12b17d02583428407f9bf086ea512ad73c2b93
Eva Göttert <eva.goettert@idealo.de>
2023-04-21 15:02:04 +0200

    Add first programatically solved implementation for tiny

--- src/main/kotlin/com/kata/Wardrobe.kt
+++ src/main/kotlin/com/kata/Wardrobe.kt
@@ -3,7 +3,24 @@ package com.kata
 import com.kata.Size.TINY
 
 class Wardrobe {
-    fun getAll250CmCombinations() :List<List<Size>> = listOf(listOf(TINY, TINY, TINY, TINY, TINY))
+    fun getAll250CmCombinations() : List<List<Size>> {
+        val resultList =  mutableListOf<List<Size>>()
+        val candidate = mutableListOf<Size>()
+
+        candidate.add(TINY)
+        var totalSize = candidate.sumBy { it.size }
+
+        while (totalSize<250){
+            candidate.add(TINY)
+            totalSize = candidate.sumBy { it.size }
+        }
+
+        if (totalSize == 250){
+            resultList.add(candidate)
+        }
+
+        return resultList
+    }
 }
 
 enum class Size (val size: Int, val price: Int){
