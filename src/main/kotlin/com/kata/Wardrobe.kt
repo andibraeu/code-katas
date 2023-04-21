3e3cefb48ef9340f3f4ddaaab7a41834b804b4ee
Eva Göttert <eva.goettert@idealo.de>
2023-04-21 14:33:36 +0200

    Add Wardrobe base structure

---
+++ src/main/kotlin/com/kata/Wardrobe.kt
@@ -0,0 +1,5 @@
+package com.kata
+
+class Wardrobe {
+    fun greeting() = "Hello World"
+}
---
+++ src/test/kotlin/com/kata/WardrobeTest.kt
@@ -0,0 +1,11 @@
+package com.kata
+
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.*
+
+internal class WardrobeTest {
+
+    @Test fun greeting() {
+        assertEquals("Hello World", Wardrobe().greeting())
+    }
+}
