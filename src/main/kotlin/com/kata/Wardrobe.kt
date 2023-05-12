27c31192d46c144d726424bb0492464e78691b01
Eva Göttert <eva.goettert@idealo.de>
2023-05-12 16:10:51 +0200

    Add recursion function and more test cases, one is still failing

--- src/main/kotlin/com/kata/Wardrobe.kt
+++ src/main/kotlin/com/kata/Wardrobe.kt
@@ -5,27 +5,50 @@ import com.kata.Size.TINY
 class Wardrobe {
     fun getAll250CmCombinations(): List<List<Size>> {
         val resultList = mutableListOf<List<Size>>()
+
+        Size.values().forEach {
+            val (totalSize, candidate) = findCandidate(it)
+
+            if (totalSize == 250) {
+                resultList.add(candidate)
+            }
+        }
+
+        return resultList
+    }
+
+    private fun findCandidate(size: Size): Pair<Int, List<Size>> {
         val candidate = mutableListOf<Size>()
 
-        candidate.add(TINY)
+        candidate.add(size)
         var totalSize = candidate.sumBy { it.size }
 
         while (totalSize < 250) {
             candidate.add(TINY)
             totalSize = candidate.sumBy { it.size }
         }
+        return totalSize to candidate
+    }
 
-        if (totalSize == 250) {
-            resultList.add(candidate)
-        }
 
-        return resultList
+    private fun recursion(candidate: List<Size>): List<Size>? {
+        var totalSize = candidate.sumBy { it.size }
+        if (totalSize > 250) return null
+        else if (totalSize == 250) return candidate
+        else return recursion(candidate + Size.MEDIUM)
     }
 }
 
 enum class Size(val size: Int, val price: Int) {
     TINY(50, 59),
-    SMALL(75, 62),
+
+    //    SMALL(75, 62),
     MEDIUM(100, 90),
     LARGE(120, 111)
 }
+
+// 50 50 50 50 50
+// 100 50 50 50
+// 100 100 50
+
+
--- src/test/kotlin/com/kata/WardrobeTest.kt
+++ src/test/kotlin/com/kata/WardrobeTest.kt
@@ -1,5 +1,7 @@
 package com.kata
 
+import com.kata.Size.LARGE
+import com.kata.Size.MEDIUM
 import com.kata.Size.TINY
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -10,11 +12,19 @@ internal class WardrobeTest {
     fun `find 250 sized combinations`() {
         val greeting = Wardrobe().getAll250CmCombinations()
         assertThat(greeting).contains(listOf(TINY, TINY, TINY, TINY, TINY))
+        assertThat(greeting).contains(listOf(MEDIUM, TINY, TINY, TINY))
+        assertThat(greeting).contains(listOf(MEDIUM, MEDIUM, TINY))
+    }
+
+    @Test
+    fun `120 length is never used`() {
+        val greeting = Wardrobe().getAll250CmCombinations()
+        greeting.forEach { assertThat(it).doesNotContain(LARGE) }
     }
 
     // TODO find all candidates
 
     // TODO refactor to usage of recursion
 
-    //TODO prices
+    // TODO prices
 }
