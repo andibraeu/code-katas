d312b68c1e062fff7dd6c5ebed7dc937c3671eae
Andreas Bräu <ab@andi95.de>
2023-08-11 14:45:28 +0200

    format our score with leading zeroes

--- src/main/kotlin/com/kata/ScoreKeeper.kt
+++ src/main/kotlin/com/kata/ScoreKeeper.kt
@@ -2,7 +2,13 @@ package com.kata
 
 class ScoreKeeper {
     fun getScore(): String {
-        return "000:000"
+        val scoreTeamA = formatScore(0)
+        val scoreTeamB = formatScore(0)
+        return "$scoreTeamA:$scoreTeamB"
+    }
+
+    private fun formatScore(score: Int): String {
+        return score.toString().padStart(3, '0')
     }
 
 
