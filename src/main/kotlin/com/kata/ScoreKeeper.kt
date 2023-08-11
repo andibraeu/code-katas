ee82884929b317de1506ff07ab723c3553c111c5
Eva Göttert <eva.goettert@idealo.de>
2023-08-11 14:53:06 +0200

    Add score methods for team A

--- src/main/kotlin/com/kata/ScoreKeeper.kt
+++ src/main/kotlin/com/kata/ScoreKeeper.kt
@@ -1,8 +1,23 @@
 package com.kata
 
 class ScoreKeeper {
+
+    private var scoreTeamA = 0
+
+    fun scoreTeamA1() {
+        scoreTeamA += 1
+    }
+
+    fun scoreTeamA2() {
+        scoreTeamA += 2
+    }
+
+    fun scoreTeamA3() {
+        scoreTeamA += 3
+    }
+
     fun getScore(): String {
-        val scoreTeamA = formatScore(0)
+        val scoreTeamA = formatScore(scoreTeamA)
         val scoreTeamB = formatScore(0)
         return "$scoreTeamA:$scoreTeamB"
     }
--- src/test/kotlin/com/kata/ScoreKeeperTest.kt
+++ src/test/kotlin/com/kata/ScoreKeeperTest.kt
@@ -14,4 +14,34 @@ class ScoreKeeperTest {
 
         assertThat(score).isEqualTo("000:000")
     }
+
+    @Test
+    fun addOnePointForTeamA() {
+        val scoreKeeper = ScoreKeeper()
+
+        scoreKeeper.scoreTeamA1()
+        val score = scoreKeeper.getScore()
+
+        assertThat(score).isEqualTo("001:000")
+    }
+
+    @Test
+    fun addTwoPointsForTeamA() {
+        val scoreKeeper = ScoreKeeper()
+
+        scoreKeeper.scoreTeamA2()
+        val score = scoreKeeper.getScore()
+
+        assertThat(score).isEqualTo("002:000")
+    }
+
+    @Test
+    fun addThreePointsForTeamA() {
+        val scoreKeeper = ScoreKeeper()
+
+        scoreKeeper.scoreTeamA3()
+        val score = scoreKeeper.getScore()
+
+        assertThat(score).isEqualTo("003:000")
+    }
 }
