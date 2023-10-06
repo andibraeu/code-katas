c83ea2830a404c451821d5ff68f091264ccef6d3
Andreas Bräu <ab@andi95.de>
2023-10-06 14:46:15 +0200

    Tennisgame2: increase readability

--- src/main/kotlin/TennisGame2.kt
+++ src/main/kotlin/TennisGame2.kt
@@ -1,115 +1,96 @@
 class TennisGame2(private val player1Name: String, private val player2Name: String) : TennisGame {
-    var P1point: Int = 0
-    var P2point: Int = 0
+    var p1point: Int = 0
+    var p2point: Int = 0
 
-    var P1res: String = ""
-    var P2res: String = ""
+    var p1res: String = ""
+    var p2res: String = ""
 
     override fun getScore(): String {
         var score = ""
-        if (P1point == P2point && P1point < 4) {
-            if (P1point == 0)
-                score = "Love"
-            if (P1point == 1)
-                score = "Fifteen"
-            if (P1point == 2)
-                score = "Thirty"
+        if (p1point == p2point && p1point < 4) {
+            when (p1point) {
+                0 -> score = "Love"
+                1 -> score = "Fifteen"
+                2 -> score = "Thirty"
+            }
             score += "-All"
         }
-        if (P1point == P2point && P1point >= 3)
+        if (p1point == p2point && p1point >= 3)
             score = "Deuce"
 
-        if (P1point > 0 && P2point == 0) {
-            if (P1point == 1)
-                P1res = "Fifteen"
-            if (P1point == 2)
-                P1res = "Thirty"
-            if (P1point == 3)
-                P1res = "Forty"
+        if (p1point > 0 && p2point == 0) {
+            when (p1point) {
+                1 -> p1res = "Fifteen"
+                2 -> p1res = "Thirty"
+                3 -> p1res = "Forty"
+            }
 
-            P2res = "Love"
-            score = "$P1res-$P2res"
+            p2res = "Love"
+            score = "$p1res-$p2res"
         }
-        if (P2point > 0 && P1point == 0) {
-            if (P2point == 1)
-                P2res = "Fifteen"
-            if (P2point == 2)
-                P2res = "Thirty"
-            if (P2point == 3)
-                P2res = "Forty"
+        if (p2point > 0 && p1point == 0) {
+            when (p2point) {
+                1 -> p2res = "Fifteen"
+                2 -> p2res = "Thirty"
+                3 -> p2res = "Forty"
+            }
 
-            P1res = "Love"
-            score = "$P1res-$P2res"
+            p1res = "Love"
+            score = "$p1res-$p2res"
         }
 
-        if (P1point > P2point && P1point < 4) {
-            if (P1point == 2)
-                P1res = "Thirty"
-            if (P1point == 3)
-                P1res = "Forty"
-            if (P2point == 1)
-                P2res = "Fifteen"
-            if (P2point == 2)
-                P2res = "Thirty"
-            score = "$P1res-$P2res"
+        if (p1point > p2point && p1point < 4) {
+            when (p1point) {
+                2 -> p1res = "Thirty"
+                3 -> p1res = "Forty"
+            }
+            when (p2point) {
+                1 -> p2res = "Fifteen"
+                2 -> p2res = "Thirty"
+            }
+            score = "$p1res-$p2res"
         }
-        if (P2point > P1point && P2point < 4) {
-            if (P2point == 2)
-                P2res = "Thirty"
-            if (P2point == 3)
-                P2res = "Forty"
-            if (P1point == 1)
-                P1res = "Fifteen"
-            if (P1point == 2)
-                P1res = "Thirty"
-            score = "$P1res-$P2res"
+        if (p2point > p1point && p2point < 4) {
+            when (p2point) {
+                2 -> p2res = "Thirty"
+                3 -> p2res = "Forty"
+            }
+            when (p1point) {
+                1 -> p1res = "Fifteen"
+                2 -> p1res = "Thirty"
+            }
+            score = "$p1res-$p2res"
         }
 
-        if (P1point > P2point && P2point >= 3) {
+        if (p1point > p2point && p2point >= 3) {
             score = "Advantage $player1Name"
         }
 
-        if (P2point > P1point && P1point >= 3) {
+        if (p2point > p1point && p1point >= 3) {
             score = "Advantage $player2Name"
         }
 
-        if (P1point >= 4 && P2point >= 0 && P1point - P2point >= 2) {
+        if (p1point >= 4 && p2point >= 0 && p1point - p2point >= 2) {
             score = "Win for $player1Name"
         }
-        if (P2point >= 4 && P1point >= 0 && P2point - P1point >= 2) {
+        if (p2point >= 4 && p1point >= 0 && p2point - p1point >= 2) {
             score = "Win for $player2Name"
         }
         return score
     }
 
-    fun SetP1Score(number: Int) {
-
-        for (i in 0 until number) {
-            P1Score()
-        }
-
+    fun setP1Score(number: Int) {
+        p1point += number
     }
 
-    fun SetP2Score(number: Int) {
-
-        for (i in 0 until number) {
-            P2Score()
-        }
-
-    }
-
-    fun P1Score() {
-        P1point++
-    }
-
-    fun P2Score() {
-        P2point++
+    fun setP2Score(number: Int) {
+        p2point += number
     }
 
     override fun wonPoint(player: String) {
         if (player === player1Name)
-            P1Score()
+            setP1Score(1)
         else
-            P2Score()
+            setP2Score(1)
     }
 }
