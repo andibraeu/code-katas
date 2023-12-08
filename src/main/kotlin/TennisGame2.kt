a107fc12f7bc793de316aff2f9a7bf41d94216d5
Eva Göttert <eva.goettert@idealo.de>
2023-12-08 14:00:23 +0100

    TennisGame2: remove line breaks

--- src/main/kotlin/TennisGame2.kt
+++ src/main/kotlin/TennisGame2.kt
@@ -4,27 +4,13 @@ class TennisGame2(private val player1Name: String, private val player2Name: Stri
 
     override fun getScore(): String {
         return when {
-            p1point >= 4 && p2point >= 0 && p1point - p2point >= 2 ->
-                "Win for $player1Name"
-
-            p2point >= 4 && p1point >= 0 && p2point - p1point >= 2 ->
-                "Win for $player2Name"
-
-            p1point == p2point && p1point >= 3 ->
-                "Deuce"
-
-            p1point == p2point ->
-                "${resolvePointsToNames(p1point)}-All"
-
-            p1point < 4 && p2point < 4 ->
-                "${resolvePointsToNames(p1point)}-${resolvePointsToNames(p2point)}"
-
-            p1point > p2point && p2point >= 3 ->
-                "Advantage $player1Name"
-
-            p2point > p1point && p1point >= 3 ->
-                "Advantage $player2Name"
-
+            p1point >= 4 && p2point >= 0 && p1point - p2point >= 2 -> "Win for $player1Name"
+            p2point >= 4 && p1point >= 0 && p2point - p1point >= 2 -> "Win for $player2Name"
+            p1point == p2point && p1point >= 3 -> "Deuce"
+            p1point == p2point -> "${resolvePointsToNames(p1point)}-All"
+            p1point < 4 && p2point < 4 -> "${resolvePointsToNames(p1point)}-${resolvePointsToNames(p2point)}"
+            p1point > p2point && p2point >= 3 -> "Advantage $player1Name"
+            p2point > p1point && p1point >= 3 -> "Advantage $player2Name"
             else -> ""
         }
     }
