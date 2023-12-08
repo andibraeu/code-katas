398a50d0fbbeb08273e83df26ebe9370a5142194
Eva Göttert <eva.goettert@idealo.de>
2023-12-08 13:55:55 +0100

    TennisGame2: use else if statements for everything

--- src/main/kotlin/TennisGame2.kt
+++ src/main/kotlin/TennisGame2.kt
@@ -4,28 +4,22 @@ class TennisGame2(private val player1Name: String, private val player2Name: Stri
 
     override fun getScore(): String {
         var score = ""
-
-        if (p1point == p2point && p1point >= 3)
-            score = "Deuce"
-        else if (p1point == p2point) {
-            score = "${resolvePointsToNames(p1point)}-All"
-        }
-
-        if (p1point < 4 && p2point < 4 && p1point != p2point) {
-            score = "${resolvePointsToNames(p1point)}-${resolvePointsToNames(p2point)}"
-        }
-
-        if (p1point > p2point && p2point >= 3) {
-            score = "Advantage $player1Name"
-        } else if (p2point > p1point && p1point >= 3) {
-            score = "Advantage $player2Name"
-        }
-
         if (p1point >= 4 && p2point >= 0 && p1point - p2point >= 2) {
             score = "Win for $player1Name"
         } else if (p2point >= 4 && p1point >= 0 && p2point - p1point >= 2) {
             score = "Win for $player2Name"
-        }
+        } else if (p1point == p2point && p1point >= 3)
+            score = "Deuce"
+        else if (p1point == p2point)
+            score = "${resolvePointsToNames(p1point)}-All"
+        else if (p1point < 4 && p2point < 4)
+            score = "${resolvePointsToNames(p1point)}-${resolvePointsToNames(p2point)}"
+        else if (p1point > p2point && p2point >= 3)
+            score = "Advantage $player1Name"
+        else if (p2point > p1point && p1point >= 3)
+            score = "Advantage $player2Name"
+
+
         return score
     }
 
