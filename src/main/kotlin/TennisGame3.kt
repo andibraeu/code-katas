472564f2f7e175d155b62bdf27e77e5c79ad48ce
Andreas Bräu <ab@andi95.de>
2023-12-15 14:48:16 +0100

    inline playername

--- src/main/kotlin/TennisGame3.kt
+++ src/main/kotlin/TennisGame3.kt
@@ -2,7 +2,7 @@ class TennisGame3(private val p1N: String, private val p2N: String) : TennisGame
 
     private var p2: Int = 0
     private var p1: Int = 0
-    private val p = mapOf(
+    private val pointToNames = mapOf(
         0 to "Love",
         1 to "Fifteen",
         2 to "Thirty",
@@ -10,14 +10,15 @@ class TennisGame3(private val p1N: String, private val p2N: String) : TennisGame
 
     override fun getScore(): String {
         return if (p1 < 4 && p2 < 4 && (p1 != 3 || p2 != 3)) {
-            if (p1 == p2) "${p[p1]}-All" else "${p[p1]}-${p[p2]}"
+            if (p1 == p2) "${pointToNames[p1]}-All" else "${pointToNames[p1]}-${pointToNames[p2]}"
         } else if (p1 == p2)
             "Deuce"
         else {
-            val playerName = if (p1 > p2) p1N else p2N
-            if ((p1 - p2) * (p1 - p2) == 1) "Advantage $playerName" else "Win for $playerName"
+            if ((p1 - p2) * (p1 - p2) == 1) "Advantage ${getPlayerName()}" else "Win for ${getPlayerName()}"
         }
     }
+
+    private fun getPlayerName() = if (p1 > p2) p1N else p2N
 
     override fun wonPoint(playerName: String) {
         if (playerName === p1N)
