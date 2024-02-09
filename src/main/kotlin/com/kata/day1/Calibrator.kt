5bbf13d72d88dfef4a6eb2965f2de45e8261f32f
Eva Göttert <eva.goettert@idealo.de>
2024-02-09 14:41:31 +0100

    cleanup

--- src/main/kotlin/com/kata/day1/Calibrator.kt
+++ src/main/kotlin/com/kata/day1/Calibrator.kt
@@ -1,17 +1,14 @@
 package com.kata.day1
 
 class Calibrator {
+    fun processList(lines: List<String>) =
+        lines.sumOf { extractTwoDigits(it) }
 
-    fun extractTwoDigits(line: String) =
+    internal fun extractTwoDigits(line: String) =
         findDigits(line).first() * 10 + findDigits(line).last()
 
-    fun findDigits(line: String): List<Int> {
-        return line.toCharArray()
+    internal fun findDigits(line: String) =
+        line.toCharArray()
             .filter { it.isDigit() }
             .map { it.digitToInt() }
-    }
-
-    fun processList(lines: List<String>) =
-        lines.map { extractTwoDigits(it) }.sum()
-
 }
