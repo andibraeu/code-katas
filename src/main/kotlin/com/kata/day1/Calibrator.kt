b7661624121b4bb3f13848d0a3d6afe90e7459df
Andreas Bräu <ab@andi95.de>
2024-02-16 13:44:02 +0100

    first attempt to convert words to digits

--- src/main/kotlin/com/kata/day1/Calibrator.kt
+++ src/main/kotlin/com/kata/day1/Calibrator.kt
@@ -15,8 +15,22 @@ class Calibrator {
             .map { it.digitToInt() }
 
     internal fun extractTwoDigitsFromStringsAndDigits(line: String): Int {
-        val convertedLine = line.replace("one", "1")
-        return extractTwoDigits(convertedLine)
+        val wordToDigitMap = mapOf(
+            "one" to  "1",
+            "two" to  "2",
+            "three" to  "3",
+            "four" to  "4",
+            "five" to  "5",
+            "six" to  "6",
+            "seven" to  "7",
+            "eight" to  "8",
+            "nine" to  "9",
+        )
+        val firstDigitWord = line.findAnyOf(wordToDigitMap.keys)?.second
+        val lastDigitWord = line.findLastAnyOf(wordToDigitMap.keys)?.second
+        val convertedLine = firstDigitWord?.let { line.replace(it, wordToDigitMap[firstDigitWord]!!) } ?: line
+        val finalLine = lastDigitWord?.let { convertedLine.replace(it, wordToDigitMap[lastDigitWord]!!) } ?: convertedLine
+        return extractTwoDigits(finalLine)
     }
 }
 
