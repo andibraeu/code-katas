5f3bf07a4d50c60206576ec17449cf98cc47d706
Andreas Bräu <ab@andi95.de>
2024-03-01 13:57:12 +0100

    verify all grabs

--- src/test/kotlin/com/kata/day2/Day2Test.kt
+++ src/test/kotlin/com/kata/day2/Day2Test.kt
@@ -24,17 +24,18 @@ class Day2Test {
 
     @ParameterizedTest
     @MethodSource("gameIdToExtract")
-    fun splitGameAndGrabs(line: String, result: Int) {
+    fun splitGameAndGrabs(line: String, grabs: String, result: Int) {
         val game = Day2().splitGameIdAndGrabs(line)
         assertThat(game.id).isEqualTo(result)
+        assertThat(game.grabs).isEqualTo(grabs)
     }
 
     private fun gameIdToExtract() = Stream.of(
-        Arguments.of("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", 1),
-        Arguments.of("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", 2),
-        Arguments.of("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", 3),
-        Arguments.of("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", 4),
-        Arguments.of("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", 5),
+        Arguments.of("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", " 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", 1),
+        Arguments.of("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", " 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", 2),
+        Arguments.of("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", " 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", 3),
+        Arguments.of("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", " 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", 4),
+        Arguments.of("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", " 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", 5),
     )
 
 }
