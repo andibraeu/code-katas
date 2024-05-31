a29c8cf9f0ac00e5dc09ef6c7be81fc6e611cf2f
Eva Göttert <eva.goettert@idealo.de>
2024-05-31 14:32:43 +0200

    fix test for Game objects

--- src/test/kotlin/com/kata/day2/Day2Test.kt
+++ src/test/kotlin/com/kata/day2/Day2Test.kt
@@ -32,11 +32,11 @@ class Day2Test {
     }
 
     private fun gameIdToExtract() = Stream.of(
-        Arguments.of("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", listOf(" 3 blue, 4 red", " 1 red, 2 green, 6 blue", " 2 green"), 1),
-        Arguments.of("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", listOf(" 1 blue, 2 green", " 3 green, 4 blue, 1 red", " 1 green, 1 blue"), 2),
-        Arguments.of("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", listOf(" 8 green, 6 blue, 20 red", " 5 blue, 4 red, 13 green", " 5 green, 1 red"), 3),
-        Arguments.of("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", listOf(" 1 green, 3 red, 6 blue", " 3 green, 6 red", " 3 green, 15 blue, 14 red"), 4),
-        Arguments.of("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", listOf(" 6 red, 1 blue, 3 green", " 2 blue, 1 red, 2 green"), 5),
+        Arguments.of("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", listOf(mapOf(RED to 4, BLUE to 3), mapOf(RED to 1, GREEN to 2, BLUE to 6), mapOf(GREEN to 2)), 1),
+        Arguments.of("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", listOf(mapOf(BLUE to 1 , GREEN to 2), mapOf(RED to 1, GREEN to 3, BLUE to 4), mapOf(GREEN to 1, BLUE to 1)), 2),
+        Arguments.of("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", listOf(mapOf(GREEN to 8, BLUE to 6, RED to 20 ), mapOf(BLUE to 5 , RED to 4, GREEN to 13), mapOf(GREEN to 5, RED to 1)), 3),
+        Arguments.of("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", listOf(mapOf(GREEN to 1 ,RED to 3, BLUE to 6 ), mapOf(GREEN to 3, RED to 6), mapOf(GREEN to 3, BLUE to 15, RED to 14)), 4),
+        Arguments.of("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", listOf(mapOf(RED to 6, BLUE to 1, GREEN to 3), mapOf(BLUE to 2, RED to 1, GREEN to 2)), 5),
     )
 
     @Test
