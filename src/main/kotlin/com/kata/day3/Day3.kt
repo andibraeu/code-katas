a0f2b6fe16de7ad2d5b92f095cd6ed0a1fb2122b
Andreas Bräu <ab@andi95.de>
2024-06-28 14:59:39 +0200

    Zwischenstand für nächste Woche

--- src/main/kotlin/com/kata/day3/Day3.kt
+++ src/main/kotlin/com/kata/day3/Day3.kt
@@ -5,20 +5,54 @@ import java.io.File
 class Day3 {
 
     fun processInput(inputLines: List<String>, part2: Boolean = false): Int {
+        val numbersList = inputLines.map {
+            extractNumbersWithPosition(it)
+        }
+        val symbolsList = inputLines.map {
+            extractSymbolsWithPosition(it)
+        }
+        val result = findNumbersNextToSymbols(numbersList, symbolsList)
         return 5
+    }
+
+    internal fun findNumbersNextToSymbols(
+        numbersList: List<List<NumberWithPosition>>,
+        symbolsList: List<List<SymbolWithPosition>>
+    ): List<Int> {
+        numbersList.forEachIndexed { lineNumber, numberWithPositions ->
+            numberWithPositions
+                .filter {
+                    val symbolsInCurrentLine = symbolsList[lineNumber]
+                    val realPosition = it.position.first - 1 .. it.position.last + 1
+                    symbolsInCurrentLine[0].position in it.position
+                }
+            // TODO: Zahlen merken und benachbarte Zeilen berücksichtigen
+        }
+        return emptyList()
     }
 
     fun extractNumbersWithPosition(line: String): List<NumberWithPosition> {
         val regex = Regex("\\d+")
         val findAll = regex.findAll(line, 0)
-        return findAll.map { NumberWithPosition(it.value.toInt(), it.groups[0]?.range) }.toList()
+        return findAll.map { NumberWithPosition(it.value.toInt(), it.groups[0]?.range!!) }.toList()
+    }
+
+    fun extractSymbolsWithPosition(line: String): List<SymbolWithPosition> {
+        val regex = Regex("[^.0-9]")
+        val findAll = regex.findAll(line, 0)
+        return findAll.map { SymbolWithPosition(it.value, it.groups[0]?.range!!.first) }.toList()
     }
 
 }
 
+data class SymbolWithPosition(
+    val value: String,
+    val position: Int
+)
+
 data class NumberWithPosition(
     val value: Int,
-    val position: IntRange?
+    val position: IntRange
 )
 
 fun main() {
--- src/test/kotlin/com/kata/day3/Day3Test.kt
+++ src/test/kotlin/com/kata/day3/Day3Test.kt
@@ -11,7 +11,7 @@ class Day3Test {
 
     @ParameterizedTest
     @MethodSource("extractNumbersInput")
-    fun `test line with two numbers`(input: String, expectedResult: List<NumberWithPosition>) {
+    fun `test line with numbers`(input: String, expectedResult: List<NumberWithPosition>) {
         val result = Day3().extractNumbersWithPosition(input)
 
         assertThat(result).isEqualTo(expectedResult)
@@ -23,4 +23,50 @@ class Day3Test {
         Arguments.of("...*...456", listOf(NumberWithPosition(456, 7..9))),
         Arguments.of("...*.....6", listOf(NumberWithPosition(6, 9..9))),
     )
+
+    @ParameterizedTest
+    @MethodSource("extractSymbolsInput")
+    fun `test line with symbols`(input: String, expectedResult: List<SymbolWithPosition>) {
+        val result = Day3().extractSymbolsWithPosition(input)
+
+        assertThat(result).isEqualTo(expectedResult)
+    }
+
+    private fun extractSymbolsInput(): Stream<Arguments> = Stream.of(
+        Arguments.of("467..114..", emptyList<SymbolWithPosition>()),
+        Arguments.of("...*....+.", listOf(SymbolWithPosition("*", 3), SymbolWithPosition("+", 8))),
+        Arguments.of("...*...#@/", listOf(SymbolWithPosition("*", 3), SymbolWithPosition("#", 7), SymbolWithPosition("@", 8), SymbolWithPosition("/", 9))),
+        Arguments.of(".(.*..?..$", listOf(SymbolWithPosition("(", 1), SymbolWithPosition("*", 3), SymbolWithPosition("?", 6), SymbolWithPosition("$", 9))),
+    )
+
+    @Test
+    fun `find numbers next to symbols`() {
+        val numbersList = listOf(
+            listOf(NumberWithPosition(467, 0..2), NumberWithPosition(114, 5..7)),
+            emptyList(),
+            listOf(NumberWithPosition(35, 2..3), NumberWithPosition(633, 6..8)),
+            emptyList(),
+            listOf(NumberWithPosition(617, 0..2)),
+            listOf(NumberWithPosition(58, 7..8)),
+            listOf(NumberWithPosition(592, 2..4)),
+            listOf(NumberWithPosition(755, 6..8)),
+            emptyList(),
+            listOf(NumberWithPosition(664, 1..3), NumberWithPosition(598, 5..7))
+        )
+        val symbolsList = listOf(
+            emptyList(),
+            listOf(SymbolWithPosition("*", 3)),
+            emptyList(),
+            listOf(SymbolWithPosition("#", 6)),
+            listOf(SymbolWithPosition("*", 3)),
+            listOf(SymbolWithPosition("+", 5)),
+            emptyList(),
+            emptyList(),
+            listOf(SymbolWithPosition("$", 3), SymbolWithPosition("*", 5)),
+            emptyList()
+        )
+        val result = Day3().findNumbersNextToSymbols(numbersList, symbolsList)
+
+        assertThat(result).containsExactly(467, 35, 633, 617, 592, 755, 664, 598)
+    }
 }
