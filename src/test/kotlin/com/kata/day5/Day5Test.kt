3da5a1be2819940004b16eb88729f8995484136c
Eva Göttert <eva.goettert@idealo.de>
2024-12-06 13:20:04 +0100

    Fix compile issue in test

--- src/test/kotlin/com/kata/day5/Day5Test.kt
+++ src/test/kotlin/com/kata/day5/Day5Test.kt
@@ -16,12 +16,9 @@ class Day5Test {
     }
 
     @Test
-    fun readSeedsRanges() {
+    fun readSeedRanges() {
         val seeds = Day5().readSeedRanges(listOf("seeds: 79 14 55 13", "", "seed-to-soil map:", "50 12 32"))
-        assertThat(seeds).containsExactly(
-            79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92,
-            55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67
-        )
+        assertThat(seeds).containsExactly(79..92L, 55..67L)
     }
 
     @Test
