267397e53ae65c1c34cd85b168347374a11e4514
Eva Göttert <eva.goettert@idealo.de>
2024-12-06 14:02:13 +0100

    Add pq calc - wip

--- src/main/kotlin/com/kata/day6/Day6.kt
+++ src/main/kotlin/com/kata/day6/Day6.kt
@@ -1,13 +1,59 @@
 package com.kata.day5
 
 import java.io.File
-import java.util.stream.Collectors.toList
+import kotlin.math.ceil
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+/*
+
+0 1 2  3  4  5  6 7
+0 6 10 12 12 10 6 0
+
+f(1) = 1 * (7 - 1)
+f(2) = 2 * (7 - 2)
+f(x) = x * (7 - x) = 7x - x²
+
+9 = 7x - x²
+
+0 = 7x - x² - 9
+- x² +7x -9
+0 = x² - 7x + 9
+
+0 = x² - time*x + distance
+
+pq-Formel:
+7÷2 − √(3,5^2 − 9) = 1,7
+7÷2 + √(3,5^2 − 9) = 5,3
+
+
+ - time/2 + √((time / 2)^2 − distance)
+
+ - time/2 - √((time / 2)^2 − distance)
+
+*/
 
 class Day6 {
 
     fun processInput(inputLines: List<String>, part2: Boolean = false): Long {
         return 0
     }
+
+    fun calculateZeroPoints(time: Int, distance: Int): IntRange {
+        val pqMinus = pqMinus(time, distance)
+        val first = ceil(pqMinus).toInt()
+        println("Minus $pqMinus")
+
+        val pqPlus = pqPlus(time, distance)
+        println("Plus $pqPlus")
+        val second: Int = pqPlus.toInt()
+
+        return first..second
+    }
+
+    private fun pqPlus(time: Int, distance: Int) = (time / 2.0) + sqrt((time / 2.0).pow(2) - distance)
+
+    private fun pqMinus(time: Int, distance: Int) = (time / 2.0) - sqrt((time / 2.0).pow(2) - distance)
 }
 
 
---
+++ src/test/kotlin/com/kata/day6/Day6Test.kt
@@ -0,0 +1,29 @@
+package com.kata.day6
+
+import com.kata.day5.Day6
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+class Day6Test {
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    fun calculateZeroPoints(time: Int, distance: Int, expectedResult: IntRange) {
+
+        val result: IntRange = Day6().calculateZeroPoints(time, distance)
+
+        assertThat(result).isEqualTo(expectedResult)
+
+    }
+
+    fun testData(): Stream<Arguments> =
+        Stream.of(
+//            Arguments.of(7, 9, 2..<6),
+//            Arguments.of(15, 40, 4..<12),
+            Arguments.of(30, 200, 11..<20),
+        )
+
+}
